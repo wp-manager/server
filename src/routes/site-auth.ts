081a91709f77777595e10d11eb8735acb22607e2
Darren <darren.griffin@live.co.uk>
2023-12-31 20:33:15 +0000

    Addeed middleware to add site

--- src/routes/site-auth.ts
+++ src/routes/site-auth.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import { authSite, handleAuthCallback } from "../controllers/site-auth";
 import { handleWPEngineCredentials } from "../controllers/wpengine-auth";
+import JWTUtils from "../utils/jwt";
 const router = express.Router();
 
 router.get("/callback", handleAuthCallback);
-router.get("/:uri", authSite);
+router.get("/:uri", JWTUtils.authorisedUserMiddleware, authSite);
 
-router.post('/wpengine', handleWPEngineCredentials);
+router.post('/wpengine', JWTUtils.authorisedUserMiddleware, handleWPEngineCredentials);
 
 
 export default router;
