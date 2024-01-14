e3c0f5157972f1e09d865df8346baade9e66ff55
Darren <darren.griffin@live.co.uk>
2024-01-14 19:25:44 +0000

    Moved screenshot tasks to run in background

--- src/controllers/site.ts
+++ src/controllers/site.ts
@@ -3,7 +3,6 @@ import WordpressAPI from "../utils/wordpress";
 import SiteAuth from "../models/site-auth";
 import puppeteer, { KnownDevices } from "puppeteer";
 
-
 const siteExists = async (req, res, next) => {
     let uri = req.params.uri;
     // replace http://, https://, and www. with nothing. Also remove everything after the tld
@@ -90,68 +89,21 @@ const screenshotDesktop = async (req, res) => {
         });
         return;
     }
-
-    if(req.site.desktopScreenshot && req.site.desktopScreenshotExpires && req.site.desktopScreenshotExpires > Date.now() && !req.query.refresh){
+    
+    req.setTimeout(5000);
+    
+    if (req.site.desktopScreenshot) {
         // Use existing screenshot
         res.set("Content-Type", "image/webp");
         let screenshot = Buffer.from(req.site.desktopScreenshot, "base64");
         res.send(screenshot);
         return;
+    } else {
+        res.status(400).json({
+            error: "No screenshot available",
+        });
     }
-    
-    // Take new screenshot
-
-    // Prepare browser
-    const browser = await puppeteer.launch({headless: 'new'});
-    const page = await browser.newPage();
-
-    // Load site
-    await page.goto(`https://${req.site.uri}`);
-
-    // Set viewport
-    await page.setViewport({
-        width: 1920,
-        height: 1080,
-    });
-
-    // Some sites don't load assets until an interaction is made by the user
-    // We simulate a scroll to ensure everything is loaded
-    await page.mouse.wheel({ deltaY: 100 });
-    await new Promise((resolve) => setTimeout(resolve, 50));
-    await page.mouse.wheel({ deltaY: -200 });
-
-    // Remove scrollbars
-    await page.addStyleTag({
-        content: "body { overflow: hidden !important; }",
-    });
-
-    // Remove unwanted elements
-    // CookieHub banner
-    await page.addStyleTag({ content: ".ch2 { display: none !important; }" });
-
-    // Let the dust settle
-    await new Promise((resolve) => setTimeout(resolve, 500));
-
-    // Take screenshot
-    const screenshot = await page.screenshot({
-        type: "webp",
-        quality: 80,
-        encoding: "base64",
-    });
-
-    // Close browser
-    await browser.close();
-
-    // Store screenshot and set the expiry date to 24 hours from now
-    req.site.desktopScreenshot = screenshot;
-    req.site.desktopScreenshotExpires = Date.now() + (1000 * 60 * 60 * 24);
-    await req.site.save();
-
-    // Serve screenshot
-    const screenshotImage = Buffer.from(screenshot, "base64");
-    res.set("Content-Type", "image/webp");
-    res.send(screenshotImage);
-}
+};
 
 const screenshotMobile = async (req, res) => {
     if (!req.site || !req.site.uri) {
@@ -161,64 +113,21 @@ const screenshotMobile = async (req, res) => {
         return;
     }
 
-    if(req.site.mobileScreenshot && req.site.mobileScreenshotExpires && req.site.mobileScreenshotExpires > Date.now() && !req.query.refresh){
+    req.setTimeout(5000);
+
+    if (req.site.mobileScreenshot) {
         // Use existing screenshot
         res.set("Content-Type", "image/webp");
         let screenshot = Buffer.from(req.site.mobileScreenshot, "base64");
         res.send(screenshot);
         return;
+    } else {
+        res.status(400).json({
+            error: "No screenshot available",
+        });
     }
-    
-    // Take new screenshot
 
-    // Prepare browser
-    const browser = await puppeteer.launch({headless: 'new'});
-    const page = await browser.newPage();
-    const device = KnownDevices["iPhone 13 Pro"];
-    await page.emulate(device);
-    // Load site
-    await page.goto(`https://${req.site.uri}`);
-
-    // Set viewport
-    await page.setViewport(device.viewport);
-
-    // Some sites don't load assets until an interaction is made by the user
-    // We simulate a scroll to ensure everything is loaded
-    await page.mouse.wheel({ deltaY: 100 });
-    await new Promise((resolve) => setTimeout(resolve, 50));
-    await page.mouse.wheel({ deltaY: -200 });
-
-    // Remove scrollbars
-    await page.addStyleTag({
-        content: "body { overflow: hidden !important; }",
-    });
-
-    // Remove unwanted elements
-    // CookieHub banner
-    await page.addStyleTag({ content: ".ch2 { display: none !important; }" });
-
-    // Let the dust settle
-    await new Promise((resolve) => setTimeout(resolve, 500));
-
-    // Take screenshot
-    const screenshot = await page.screenshot({
-        type: "webp",
-        quality: 80,
-        encoding: "base64",
-    });
-
-    // Close browser
-    await browser.close();
-
-    // Store screenshot and set the expiry date to 24 hours from now
-    req.site.mobileScreenshot = screenshot;
-    req.site.mobileScreenshotExpires = Date.now() + (1000 * 60 * 60 * 24);
-    await req.site.save();
-
-    // Serve screenshot
-    const screenshotImage = Buffer.from(screenshot, "base64");
-    res.set("Content-Type", "image/webp");
-    res.send(screenshotImage);
-}
+    return;
+};
 
 export { proxySite, siteExists, screenshotDesktop, screenshotMobile };
---
+++ src/utils/screenshot.ts
@@ -0,0 +1,175 @@
+import puppeteer, { KnownDevices } from "puppeteer";
+import Site from "../models/site";
+
+class ScreenshotWorker {
+    inProgress = [];
+    currentSite = null;
+    maxConcurrency = process.env.MAX_PARALLEL_SCREENSHOTS || 2;
+
+    async start() {
+        this.doJob();
+        setInterval(() => {
+            this.doJob();
+        }, 1 * 1000);
+    }
+
+    async doJob() {
+        if (this.inProgress.length > 2) {
+            return;
+        }
+
+        const site = await this.getSite();
+
+        if (!site) {
+            return;
+        }
+
+        this.inProgress.push(site.uri);
+
+        await this.processSite(site);
+        this.inProgress = this.inProgress.filter((uri) => uri !== site.uri);
+    }
+
+    async getSite() {
+        const sites = await Site.find({
+            $or: [
+                {
+                    mobileScreenshot: null,
+                },
+                {
+                    desktopScreenshot: null,
+                },
+                {
+                    screenshotExpires: null,
+                },
+                {
+                    screenshotExpires: {
+                        $lt: Date.now(),
+                    },
+                },
+            ],
+            // Ignore sites that are already in progress
+            uri: {
+                $nin: this.inProgress,
+            },
+        })
+            .sort({
+                screenshotExpires: 1,
+            })
+            .limit(1);
+
+        if (sites.length === 0) {
+            return;
+        }
+
+        return sites[0];
+    }
+
+    async processSite(site) {
+        console.log(`[${site.uri}] Processing screenshots`);
+
+        if (
+            !site.mobileScreenshot ||
+            !site.desktopScreenshot ||
+            !site.screenshotExpires ||
+            site.screenshotExpires < Date.now()
+        ) {
+            console.log(`[${site.uri}] Capturing screenshots`);
+            await this.captureScreenshots(site);
+            console.log(`[${site.uri}] Captured screenshots`);
+
+            site.screenshotExpires = new Date(Date.now() + 24 * 60 * 60 * 1000);
+            await site.save();
+        }
+
+        site = null;
+    }
+
+    async captureScreenshots(site) {
+        // Prepare browser
+        const browser = await puppeteer.launch({ headless: 'new' });
+
+        try {
+            const page = await browser.newPage();
+
+            // Load site
+            await page.goto(`https://${site.uri}`);
+
+            // Set viewport
+            await page.setViewport({
+                width: 1920,
+                height: 1080,
+            });
+
+            // Some sites don't load assets until an interaction is made by the user
+            // We simulate a scroll to ensure everything is loaded
+            await page.mouse.wheel({ deltaY: 100 });
+            await new Promise((resolve) => setTimeout(resolve, 50));
+            await page.mouse.wheel({ deltaY: -200 });
+
+            // Remove scrollbars
+            await page.addStyleTag({
+                content: "body { overflow: hidden !important; }",
+            });
+
+            // Wait for a second
+            await new Promise((resolve) => setTimeout(resolve, 0));
+
+            // Remove unwanted elements
+            // CookieHub banner
+            await page.addStyleTag({
+                content: ".ch2 { display: none !important; }",
+            });
+
+            // Let the dust settle
+            await new Promise((resolve) => setTimeout(resolve, 0));
+
+            // Take screenshot
+            const desktopScreenshot = await page.screenshot({
+                type: "webp",
+                quality: 80,
+                encoding: "base64",
+            });
+
+            site.desktopScreenshot = desktopScreenshot;
+            await site.save();
+
+            // Mobile
+            const device = KnownDevices["iPhone 13 Pro"];
+            await page.emulate(device);
+
+            // Set viewport
+            await page.setViewport(device.viewport);
+
+            // Scroll to top just in case
+            await page.mouse.wheel({ deltaY: -1000 });
+
+            // Wait for a second
+            await new Promise((resolve) => setTimeout(resolve, 0));
+
+            // Take screenshot
+            const mobileScreenshot = await page.screenshot({
+                type: "webp",
+                quality: 80,
+                encoding: "base64",
+            });
+
+            // Close browser
+            await browser.close();
+
+            site.mobileScreenshot = mobileScreenshot;
+            site.screenshotExpires = new Date(Date.now() + 24 * 60 * 60 * 1000);
+
+            await site.save();
+        } catch (e) {
+            await browser.close();
+            console.log(
+                `[${site.uri}] Failed to capture screenshots. Will retry in 30 minutes`
+            );
+            site.screenshotExpires = new Date(Date.now() + 30 * 60 * 1000);
+            await site.save();
+        }
+    }
+}
+
+export default ScreenshotWorker;
