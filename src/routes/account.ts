437bd5ce506f880499b1aaa4592f5051a6f885f3
Darren Griffin <darren.griffin@live.co.uk>
2024-01-30 23:32:40 +0000

    Added fallback plugin proxy

--- src/routes/account.ts
+++ src/routes/account.ts
@@ -1,5 +1,11 @@
 import express from "express";
-import { getAccount, register, login, emailAvailability, logout } from "../controllers/auth";
+import {
+    getAccount,
+    register,
+    login,
+    emailAvailability,
+    logout,
+} from "../controllers/auth";
 import JWTUtils from "../utils/jwt";
 const router = express.Router();
 
@@ -9,75 +15,87 @@ router.post("/login", login);
 router.post("/logout", logout);
 router.post("/email-availability", emailAvailability);
 
-router.post("/install-plugin", JWTUtils.authorisedUserMiddleware, async (req: any, res) => {
-    const { path } = req.body;
+router.post(
+    "/install-plugin",
+    JWTUtils.authorisedUserMiddleware,
+    async (req: any, res) => {
+        const { path } = req.body;
 
-    const user = req.user;
+        const user = req.user;
 
-    if(!path) {
-        return res.status(400).json({
-            error: "Missing path",
+        if (!path) {
+            return res.status(400).json({
+                error: "Missing path",
+            });
+        }
+
+        user.plugins.push({ path });
+        await user.save();
+
+        res.json({
+            message: "Plugin installed",
         });
     }
+);
 
-    user.plugins.push({ path });
-    await user.save();
+router.post(
+    "/uninstall-plugin",
+    JWTUtils.authorisedUserMiddleware,
+    async (req: any, res) => {
+        const { path } = req.body;
 
-    res.json({
-        message: "Plugin installed",
-    });
-});
+        const user = req.user;
 
-router.post("/uninstall-plugin", JWTUtils.authorisedUserMiddleware, async (req: any, res) => {
-    const { path } = req.body;
+        if (!path) {
+            return res.status(400).json({
+                error: "Missing path",
+            });
+        }
 
-    const user = req.user;
+        user.plugins = user.plugins.filter((plugin) => {
+            return plugin.path !== path;
+        });
 
-    if(!path) {
-        return res.status(400).json({
-            error: "Missing path",
+        await user.save();
+
+        res.json({
+            message: "Plugin uninstalled",
         });
     }
+);
 
-    user.plugins = user.plugins.filter((plugin) => {
-        return plugin.path !== path;
-    });
-
-    await user.save();
-
-    res.json({
-        message: "Plugin uninstalled",
-    });
-});
-
-router.get('/plugin/:cdnurl', async (req: any, res) => {
+router.get("/plugin/:cdnurl", async (req: any, res) => {
     const { cdnurl } = req.params;
 
     // decode the url if it's encoded
     const decoded = decodeURIComponent(cdnurl);
 
-    // if token is in the decoded url, then it's a private plugin  
+    // if token is in the decoded url, then it's a private plugin
     // Take the token out of the url, and use it as a bearer token
     const url = new URL(decoded);
-    const token = url.searchParams.get('token');
+    const token = url.searchParams.get("token");
 
-    if(token) {
-        const response = await fetch(decoded, {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        });
-
-        if(!response.ok) {
-            return res.status(404).json({
-                error: "Plugin not found",
-            });
-        }
-
-        res.setHeader('Content-Type', 'application/javascript');
-        res.send(await response.text());
-        return;
+    let params = {
+        headers: {},
+    };
+    
+    if (token) {
+        params.headers = {
+            Authorization: `Bearer ${token}`,
+        };
     }
+
+    const response = await fetch(decoded, params);
+
+    if (!response.ok) {
+        return res.status(404).json({
+            error: "Plugin not found",
+        });
+    }
+
+    res.setHeader("Content-Type", "application/javascript");
+    res.send(await response.text());
+    return;
 });
 
 export default router;
